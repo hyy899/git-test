c948910bcade0acdcf0dbe9a7291360b29a45d3a
huangyuyun <113455193+hyy899@users.noreply.github.com>
2023-11-02 18:55:29 +0800

    master merge

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,6 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
-        System.out.println("Hello hot-fix");
+        System.out.println("Hello master");
     }
 }
